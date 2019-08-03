ccea12578a7a98facc86d81b46b07e93cf70ad5a
Alex Korolenko <28filosof28@gmail.com>
2019-08-03 17:00:06 +0300

    Version 3.1
    Add comments and small tips

--- common.js
+++ common.js
@@ -1,45 +1,58 @@
+//функция генерации случайных координат от 0 до 3
 function generateNumber() {
 	let x = Math.floor(0 + Math.random() * (3 + 1)),
 			y =	Math.floor(0 + Math.random() * (3 + 1));
 	return [x, y];
 };
 
+//функция генерации новой ячейки на поле
 function createNumber() {
+	//проверка на то осталось ли место на поле для новой ячейки
 	if(!document.querySelector('[data-number = "0"]')) return alert("Игра окончена!");
 	
-	let numberCoordinates = generateNumber();
-	let number = document.querySelector('[data-posX = "' + numberCoordinates[0] + '"][data-posY = "' +
+	//массив со случайными координатами
+	let numberCoordinates = generateNumber(),
+	//ячейка находящаяся по случайно сгенерированным координатам
+			number = document.querySelector('[data-posX = "' + numberCoordinates[0] + '"][data-posY = "' +
 		numberCoordinates[1] + '"]');
-
+	//цикл закончится тогда когда будет выбрана пустая ячейка 
 	while(number.getAttribute('data-number') != 0) {
 		numberCoordinates = generateNumber();
 		number = document.querySelector('[data-posX = "' + numberCoordinates[0] + '"][data-posY = "' +
 			numberCoordinates[1] + '"]');
 	};
 
+	//занесение начальнх данных в пустую ячейку
 	number.innerHTML = 2;
 	number.setAttribute('data-number', '2');
-	//alert(number.getAttribute("data-posX") + " " + number.getAttribute("data-posY"));
 };
 
+//обработка события клика на кнопку
 document.addEventListener('click', function (event) {
+	//проверка на то нажата ли нужная кнопка
 	if (!event.target.classList.contains('start-game')) return;
 
+	//массив со всеми ячейками
 	let elements = document.getElementsByClassName('element');
 
+	//обнуление всех ячеек
 	for(let i = 0; i < elements.length; i++) {
 		elements[i].innerHTML = "";
 		elements[i].setAttribute('data-number', '0'); 
+		//установка фиксированной высоты (квадрат)
 		elements[i].style.height = elements[i].clientWidth + "px";
 	}
 
+	//генерация новой ячейки
 	createNumber();
 });
 
+//функция поиска элемента по указанным координатам
 function findElem(x, y) {
 	return document.querySelector('[data-posX = "' + x + '"][data-posY = "' + y + '"]');
 }
 
+//функция приведения атрибутов data-sum и data-move к стандартному виду 
 function sumAbort() {
 	let elemSum = document.querySelectorAll('[data-sum = "false"]'),
 			elemMove = document.querySelectorAll('[data-move = "false"]');
@@ -49,8 +62,11 @@ function sumAbort() {
 		elemMove[i].setAttribute("data-move", "true");
 }
 
+//функция удаления пустых ячеек по направлению движения
 function delZero(x, y, direction) {
+	//массив элементов из выбранной строки
 	let elems = [];
+	//выбор направления перемещения
 	if (direction == "right") {
 		elems.push(findElem(x, y));
 		elems.push(findElem(x - 1, y));
@@ -64,25 +80,31 @@ function delZero(x, y, direction) {
 		elems.push(findElem(x, y - 1));
 		elems.push(findElem(x, y));
 	}
+	//в случае если у ячейки по выбранному направлению есть пустая ячейка 
 	if (elems[0].innerHTML == "" && elems[1].innerHTML != "") {
+		//занесение значения из ячейки в переменную и в пустую ячейку
 		let sum = elems[1].getAttribute("data-number");
 		elems[0].setAttribute("data-number", sum);
 		if (sum == 0) {
 			elems[0].innerHTML = "";
-			//alert(2);
 		} else {
 			elems[0].innerHTML = sum;
-			//alert(3);
 		} 
+		//обнуление выбранной ячейки
 		elems[1].innerHTML = "";
 		elems[1].setAttribute("data-number", 0);
+		//тригер на, то что произошло движение
 		elems[1].setAttribute("data-move", "false");
 	}
 }
 
+//функция перемещения элементов
 function moveElem(i, j, direction) {
+	//массив элементов из выбранной строки
 	let elems = [];
+	//занесение начального элемента в массив
 	elems.push(findElem(i, j));
+	//выбор направления и занесение в массив дополнительных элементов
 	if (direction == "right") {
 		elems.push(findElem(i + 1, j));
 		if (i <= 1) elems.push(findElem(i + 2, j));
@@ -96,30 +118,39 @@ function moveElem(i, j, direction) {
 		elems.push(findElem(i, j - 1));
 		if (j >= 2) elems.push(findElem(i, j - 2));
 	}; 
+	//условия для суммирования двух одинаковых ячеек
 	if ((elems[0].getAttribute("data-number") == elems[1].getAttribute("data-number")) && 
 			(elems[0].getAttribute("data-number") != 0 && elems[1].getAttribute("data-number") != 0) && 
 			(elems[0].getAttribute("data-sum") == "true" && elems[1].getAttribute("data-sum") == "true")) {
+		//переменная с значением выбранной ячейки
 		let sum = elems[1].getAttribute("data-number");
 		
+		//функция сумирования одинаковых элементов по направлению движения
 		function sumElem(i) {
+			//суммирование и занесение данных в ячейку
 			sum *= 2;
 			elems[1 + i].setAttribute("data-number", sum);
+			//тригер на, то что произошло суммирование
 			elems[1 + i].setAttribute("data-sum", false);
 			elems[1 + i].innerHTML = sum;
+			//обнуление второй ячейки
 			elems[0 + i].setAttribute("data-number", 0);
 			elems[0 + i].innerHTML = "";
 		}
 
+		//условия, если стоят 3 одинаковых элемента в ряд
 		if (elems[2]) {
 			if (elems[2].getAttribute("data-number") == sum) {
 				sumElem(1);
+			//прочие случаи
 			} else {
 				sumElem(0);
 			}
+		//прочие случаи
 		} else {
 			sumElem(0);
 		}
-		//alert(1);
+	//если суммирование не нужно, то вызов функции по удалению пустых ячеек на пути движения
 	} else {
 		if (direction == "right") {
 			delZero(1, j, direction);
@@ -141,45 +172,60 @@ function moveElem(i, j, direction) {
 	}
 }
 
+//событие на клавиши перемещения
 document.addEventListener('keydown', function (event) {
-	if (event.keyCode >= 37 && event.keyCode <= 40) {
-		let elements = document.getElementsByClassName('element');
-	} else {
-		return;
-	}
+	//проверка на нажатие необходимых клавиш
+	if (event.keyCode < 37 || event.keyCode > 40) return;
 
 	//если нажата стрелочка ← 
 	if (event.keyCode == 37) {
-		for(let j = 0; j < 4; j++)
+		for(let j = 0; j < 4; j++) {
 			for(let i = 3; i > 0; i--) {
 				moveElem(i, j, "left");
 			}
+			delZero(3, j, "left");
+			delZero(2, j, "left");
+			delZero(1, j, "left");
+		}
+			
 	}
 	//если нажата стрелочка ↑
 	if (event.keyCode == 38) {
 		for(let j = 3; j > 0; j--)
 			for(let i = 0; i < 4; i++) {
 				moveElem(i, j, "up");
+				delZero(i, 3, "up");
+				delZero(i, 2, "up");
+				delZero(i, 1, "up");
 			}
 	}
 	//если нажата стрелочка →
 	if (event.keyCode == 39) {
-		for(let j = 0; j < 4; j++)
+		for(let j = 0; j < 4; j++) {
 			for(let i = 0; i < 3; i++) {
 				moveElem(i, j, "right");
 			}
+			delZero(1, j, "right");
+			delZero(2, j, "right");
+			delZero(3, j, "right");
+		}	
 	}
 	//если нажата стрелочка ↓
 	if (event.keyCode == 40) {
 		for(let j = 0; j < 3; j++)
 			for(let i = 0; i < 4; i++) {
 				moveElem(i, j, "down");
+				delZero(i, 1, "down");
+				delZero(i, 2, "down");
+				delZero(i, 3, "down");
 			}
 	}
+
+	//генерация новой ячейки, если произошло действие
 	if (document.querySelector('[data-sum = "false"]') || document.querySelector('[data-move = "false"]')) {
-		//alert("gg");
 		createNumber();
 	} 
+	//вызов функции для занесения в определенные атрибуты значений по умолчанию
 	sumAbort();
 });
 
